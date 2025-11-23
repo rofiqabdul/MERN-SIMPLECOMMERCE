e8eb1502dc0ba5b9de4ab950c163a30438525c03
rofiqabdul <rofiqabdul04@gmail.com>
2025-11-23 21:16:19 +0700

    #9 - Coding authController part 1

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,3 +3,40 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { successRespone, errorResponse } from "../utils/response.js";
 import cookieOptions from "../utils/cookieOptions.js";
+
+export const register = async (req, res) => {
+	const { name, email, password } = req.body;
+
+	const existedEmail = prisma.user.findUnique({ where: { email } });
+	if (existedEmail)
+		return errorResponse(res, "email is already in use!", null, 400);
+
+	// hash password before sending to database
+	const hasedPassword = bcrypt(password, 10);
+
+	const user = await prisma.user.create({
+		data: {
+			name,
+			email,
+			password: hasedPassword,
+		},
+	});
+
+	return successRespone(res, "Refister Successful", {
+		id: user.id,
+		name: user.name,
+		email: user.email,
+	});
+};
+
+export const loign = async (req, res) => {
+	const { email, password } = req.body;
+
+	const user = await prisma.user.findUnique({ where: { email } });
+	if (!user) return errorResponse(res, "Email tidak ditemukan!", null, 401);
+
+	const userMatch = await bcrypt.compare(password, user.password);
+	if (!userMatch) return errorResponse(res, "Password salah!", null, 401);
+};
+
+const logout = () => {};
