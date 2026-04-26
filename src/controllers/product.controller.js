628d239249d4ff8217121dffae61f906f8e35a55
rofiqabdul <rofiqabdul04@gmail.com>
2026-04-27 02:52:20 +0700

    #16 - Coding product part 3

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -29,12 +29,111 @@ export const getAllProduct = async (req, res) => {
 		);
 	}
 };
-// getProductByInventoryId,
-export const getProductByInventoryId = async (req, res) => {};
-// getProductById,
-export const getProductById = async (req, res) => {};
-// createProduct,
-export const createProduct = async (req, res) => {};
+
+export const getProductByInventoryId = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const product = await prisma.product.findMany({
+			where: { inventoryId: id },
+		});
+
+		if (!product || product.length === 0) {
+			return errorResponse(
+				res,
+				"No product found on this inventory",
+				null,
+				404
+			);
+		}
+
+		const base = `${req.protocol}://${req.get("host")}`;
+		const productWithImageUrl = product.map((item) => ({
+			...item,
+			image: item.image ? cleanImageUrl(base, item.image) : null,
+		}));
+
+		return successRespone(
+			res,
+			"Get product by inventory id successful",
+			productWithImageUrl
+		);
+	} catch (error) {
+		return errorResponse(
+			res,
+			"Get product failed",
+			{ error: error.message },
+			500
+		);
+	}
+};
+
+export const getProductById = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const product = await prisma.product.findUnique({
+			where: { id },
+		});
+
+		if (!product) {
+			return errorResponse(res, "Product not found", null, 404);
+		}
+
+		const base = `${req.protocol}://${req.get("host")}`;
+		const productWithImageUrl = product.map((item) => ({
+			...item,
+			image: item.image ? cleanImageUrl(base, item.image) : null,
+		}));
+
+		return successRespone(
+			res,
+			"Get product by id successful",
+			productWithImageUrl
+		);
+	} catch (error) {
+		return errorResponse(
+			res,
+			"Get product by id failed",
+			{ error: error.message },
+			500
+		);
+	}
+};
+
+export const createProduct = async (req, res) => {
+	try {
+		const { name, price, stock, description, inventoryId } = req.body;
+		const image = req.file ? `/uploads/${req.file.filename}` : null;
+
+		const product = await prisma.product.create({
+			data: {
+				name,
+				price: parseFloat(price),
+				stock: parseFloat(stock),
+				description,
+				image,
+				inventoryId,
+			},
+		});
+
+		if (!product) {
+			return errorResponse(res, "Product not found", null, 404);
+		}
+
+		const baseUrl = `${req.protocol}://${req.get("host")}`;
+
+		return successRespone(res, "Create product successful", {
+			...product,
+			image: product.image ? `${baseUrl}${product.image}` : null,
+		});
+	} catch (error) {
+		return errorResponse(
+			res,
+			"Create product failed",
+			{ error: error.message },
+			500
+		);
+	}
+};
 // updateProduct,
 export const updateProduct = async (req, res) => {};
 // deleteProduct,
